a3539be5f1a2a0a5d26ec2b936d04b466e926cef
MOHAMAD HASSAN SWEID <alkaceh.h2@gmail.com>
2024-12-18 15:12:28 +0300

    Add files via upload

---
+++ tek-tak-tok/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace tek_tak_tok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_x_o_1_3 = new System.Windows.Forms.Button();
+            this.button_x_o_1_2 = new System.Windows.Forms.Button();
+            this.button_x_o_1_1 = new System.Windows.Forms.Button();
+            this.button_x_o_2_2 = new System.Windows.Forms.Button();
+            this.button_x_o_3_2 = new System.Windows.Forms.Button();
+            this.button_x_o_2_1 = new System.Windows.Forms.Button();
+            this.button_x_o_3_1 = new System.Windows.Forms.Button();
+            this.button_x_o_2_3 = new System.Windows.Forms.Button();
+            this.button_x_o_3_3 = new System.Windows.Forms.Button();
+            this.label_turn = new System.Windows.Forms.Label();
+            this.label_player = new System.Windows.Forms.Label();
+            this.label_winer = new System.Windows.Forms.Label();
+            this.label_winer_name = new System.Windows.Forms.Label();
+            this.button_reset_gaime = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_x_o_1_3
+            // 
+            this.button_x_o_1_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_1_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_1_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_1_3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_1_3.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_1_3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_1_3.Location = new System.Drawing.Point(1211, 120);
+            this.button_x_o_1_3.Name = "button_x_o_1_3";
+            this.button_x_o_1_3.Size = new System.Drawing.Size(183, 176);
+            this.button_x_o_1_3.TabIndex = 0;
+            this.button_x_o_1_3.Tag = "13";
+            this.button_x_o_1_3.Text = "?";
+            this.button_x_o_1_3.UseVisualStyleBackColor = false;
+            this.button_x_o_1_3.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_1_2
+            // 
+            this.button_x_o_1_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_1_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_1_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_1_2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_1_2.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_1_2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_1_2.Location = new System.Drawing.Point(1011, 127);
+            this.button_x_o_1_2.Name = "button_x_o_1_2";
+            this.button_x_o_1_2.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_1_2.TabIndex = 1;
+            this.button_x_o_1_2.Tag = "12";
+            this.button_x_o_1_2.Text = "?";
+            this.button_x_o_1_2.UseVisualStyleBackColor = false;
+            this.button_x_o_1_2.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_1_1
+            // 
+            this.button_x_o_1_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_1_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_1_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_1_1.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_1_1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_1_1.Location = new System.Drawing.Point(807, 120);
+            this.button_x_o_1_1.Name = "button_x_o_1_1";
+            this.button_x_o_1_1.Size = new System.Drawing.Size(183, 176);
+            this.button_x_o_1_1.TabIndex = 2;
+            this.button_x_o_1_1.Tag = "11";
+            this.button_x_o_1_1.Text = "?";
+            this.button_x_o_1_1.UseVisualStyleBackColor = false;
+            this.button_x_o_1_1.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_2_2
+            // 
+            this.button_x_o_2_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_2_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_2_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_2_2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_2_2.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_2_2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_2_2.Location = new System.Drawing.Point(1011, 314);
+            this.button_x_o_2_2.Name = "button_x_o_2_2";
+            this.button_x_o_2_2.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_2_2.TabIndex = 3;
+            this.button_x_o_2_2.Tag = "22";
+            this.button_x_o_2_2.Text = "?";
+            this.button_x_o_2_2.UseVisualStyleBackColor = false;
+            this.button_x_o_2_2.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_3_2
+            // 
+            this.button_x_o_3_2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_3_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_3_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_3_2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_3_2.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_3_2.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_3_2.Location = new System.Drawing.Point(1011, 499);
+            this.button_x_o_3_2.Name = "button_x_o_3_2";
+            this.button_x_o_3_2.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_3_2.TabIndex = 4;
+            this.button_x_o_3_2.Tag = "32";
+            this.button_x_o_3_2.Text = "?";
+            this.button_x_o_3_2.UseVisualStyleBackColor = false;
+            this.button_x_o_3_2.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_2_1
+            // 
+            this.button_x_o_2_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_2_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_2_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_2_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_2_1.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_2_1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_2_1.Location = new System.Drawing.Point(807, 314);
+            this.button_x_o_2_1.Name = "button_x_o_2_1";
+            this.button_x_o_2_1.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_2_1.TabIndex = 5;
+            this.button_x_o_2_1.Tag = "21";
+            this.button_x_o_2_1.Text = "?";
+            this.button_x_o_2_1.UseVisualStyleBackColor = false;
+            this.button_x_o_2_1.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_3_1
+            // 
+            this.button_x_o_3_1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_3_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_3_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_3_1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_3_1.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_3_1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_3_1.Location = new System.Drawing.Point(807, 499);
+            this.button_x_o_3_1.Name = "button_x_o_3_1";
+            this.button_x_o_3_1.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_3_1.TabIndex = 6;
+            this.button_x_o_3_1.Tag = "31";
+            this.button_x_o_3_1.Text = "?";
+            this.button_x_o_3_1.UseVisualStyleBackColor = false;
+            this.button_x_o_3_1.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_2_3
+            // 
+            this.button_x_o_2_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_2_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_2_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_2_3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_2_3.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_2_3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_2_3.Location = new System.Drawing.Point(1211, 314);
+            this.button_x_o_2_3.Name = "button_x_o_2_3";
+            this.button_x_o_2_3.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_2_3.TabIndex = 7;
+            this.button_x_o_2_3.Tag = "23";
+            this.button_x_o_2_3.Text = "?";
+            this.button_x_o_2_3.UseVisualStyleBackColor = false;
+            this.button_x_o_2_3.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // button_x_o_3_3
+            // 
+            this.button_x_o_3_3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button_x_o_3_3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_x_o_3_3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_x_o_3_3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_x_o_3_3.Font = new System.Drawing.Font("Harrington", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_x_o_3_3.ForeColor = System.Drawing.Color.Fuchsia;
+            this.button_x_o_3_3.Location = new System.Drawing.Point(1211, 499);
+            this.button_x_o_3_3.Name = "button_x_o_3_3";
+            this.button_x_o_3_3.Size = new System.Drawing.Size(183, 169);
+            this.button_x_o_3_3.TabIndex = 8;
+            this.button_x_o_3_3.Tag = "33";
+            this.button_x_o_3_3.Text = "?";
+            this.button_x_o_3_3.UseVisualStyleBackColor = false;
+            this.button_x_o_3_3.Click += new System.EventHandler(this.button_x_o_Click);
+            // 
+            // label_turn
+            // 
+            this.label_turn.AutoSize = true;
+            this.label_turn.BackColor = System.Drawing.Color.Black;
+            this.label_turn.Font = new System.Drawing.Font("Niagara Solid", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_turn.ForeColor = System.Drawing.Color.Yellow;
+            this.label_turn.Location = new System.Drawing.Point(158, 127);
+            this.label_turn.Name = "label_turn";
+            this.label_turn.Size = new System.Drawing.Size(179, 128);
+            this.label_turn.TabIndex = 10;
+            this.label_turn.Text = "turn";
+            // 
+            // label_player
+            // 
+            this.label_player.AutoSize = true;
+            this.label_player.Font = new System.Drawing.Font("Niagara Solid", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_player.ForeColor = System.Drawing.Color.Lime;
+            this.label_player.Location = new System.Drawing.Point(158, 279);
+            this.label_player.Name = "label_player";
+            this.label_player.Size = new System.Drawing.Size(259, 128);
+            this.label_player.TabIndex = 12;
+            this.label_player.Text = "player1";
+            // 
+            // label_winer
+            // 
+            this.label_winer.AutoSize = true;
+            this.label_winer.Font = new System.Drawing.Font("Palace Script MT", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_winer.ForeColor = System.Drawing.Color.Yellow;
+            this.label_winer.Location = new System.Drawing.Point(161, 433);
+            this.label_winer.Name = "label_winer";
+            this.label_winer.Size = new System.Drawing.Size(219, 111);
+            this.label_winer.TabIndex = 14;
+            this.label_winer.Text = "Winer";
+            // 
+            // label_winer_name
+            // 
+            this.label_winer_name.AutoSize = true;
+            this.label_winer_name.Font = new System.Drawing.Font("Palace Script MT", 72F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_winer_name.ForeColor = System.Drawing.Color.Lime;
+            this.label_winer_name.Location = new System.Drawing.Point(195, 544);
+            this.label_winer_name.Name = "label_winer_name";
+            this.label_winer_name.Size = new System.Drawing.Size(278, 111);
+            this.label_winer_name.TabIndex = 15;
+            this.label_winer_name.Text = "prossing";
+            // 
+            // button_reset_gaime
+            // 
+            this.button_reset_gaime.BackColor = System.Drawing.Color.HotPink;
+            this.button_reset_gaime.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.button_reset_gaime.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_reset_gaime.Font = new System.Drawing.Font("Papyrus", 22.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_reset_gaime.Location = new System.Drawing.Point(451, 718);
+            this.button_reset_gaime.Name = "button_reset_gaime";
+            this.button_reset_gaime.Size = new System.Drawing.Size(539, 87);
+            this.button_reset_gaime.TabIndex = 16;
+            this.button_reset_gaime.Text = "RESET GAIME";
+            this.button_reset_gaime.UseVisualStyleBackColor = false;
+            this.button_reset_gaime.Click += new System.EventHandler(this.button_reset_gaime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1626, 883);
+            this.Controls.Add(this.button_reset_gaime);
+            this.Controls.Add(this.label_winer_name);
+            this.Controls.Add(this.label_winer);
+            this.Controls.Add(this.label_player);
+            this.Controls.Add(this.label_turn);
+            this.Controls.Add(this.button_x_o_3_3);
+            this.Controls.Add(this.button_x_o_2_3);
+            this.Controls.Add(this.button_x_o_3_1);
+            this.Controls.Add(this.button_x_o_2_1);
+            this.Controls.Add(this.button_x_o_3_2);
+            this.Controls.Add(this.button_x_o_2_2);
+            this.Controls.Add(this.button_x_o_1_1);
+            this.Controls.Add(this.button_x_o_1_2);
+            this.Controls.Add(this.button_x_o_1_3);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_x_o_1_3;
+        private System.Windows.Forms.Button button_x_o_1_2;
+        private System.Windows.Forms.Button button_x_o_1_1;
+        private System.Windows.Forms.Button button_x_o_2_2;
+        private System.Windows.Forms.Button button_x_o_3_2;
+        private System.Windows.Forms.Button button_x_o_2_1;
+        private System.Windows.Forms.Button button_x_o_3_1;
+        private System.Windows.Forms.Button button_x_o_2_3;
+        private System.Windows.Forms.Button button_x_o_3_3;
+        private System.Windows.Forms.Label label_turn;
+        private System.Windows.Forms.Label label_player;
+        private System.Windows.Forms.Label label_winer;
+        private System.Windows.Forms.Label label_winer_name;
+        private System.Windows.Forms.Button button_reset_gaime;
+    }
+}
+
---
+++ tek-tak-tok/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tek_tak_tok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color c = Color.FromArgb(255, 255, 255, 255);
+
+            Pen p = new Pen(c);
+            p.Width = 10;
+
+            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(p, 900, 100, 900, 550);
+
+            e.Graphics.DrawLine(p, 750, 100, 750, 550);
+
+            e.Graphics.DrawLine(p, 600, 250, 1050, 250);
+
+            e.Graphics.DrawLine(p, 600, 400, 1050, 400);
+        }
+
+        int end_game = 0;
+
+        private void buttons(Color c, FlatStyle fs,string s,bool f)
+        {
+            button_x_o_1_1.BackColor =   c;
+            button_x_o_1_1.FlatStyle =  fs;
+            button_x_o_1_1.Text =       s;
+            button_x_o_1_1.Enabled =    f;
+            button_x_o_1_1.ForeColor = Color.Fuchsia;
+            button_x_o_1_1.Tag = 11;
+
+            button_x_o_1_2.BackColor =  c;
+            button_x_o_1_2.FlatStyle = fs;
+            button_x_o_1_2.Text =     s;
+            button_x_o_1_2.Enabled =  f;
+            button_x_o_1_2.ForeColor = Color.Fuchsia;
+            button_x_o_1_2.Tag = 12;
+
+            button_x_o_1_3.BackColor =      c;
+            button_x_o_1_3.FlatStyle = fs;
+            button_x_o_1_3.Text =      s;
+            button_x_o_1_3.Enabled = f;
+            button_x_o_1_3.ForeColor = Color.Fuchsia;
+            button_x_o_1_3.Tag = 13;
+
+            button_x_o_2_1.BackColor =     c;
+            button_x_o_2_1.FlatStyle = fs;
+            button_x_o_2_1.Text =      s;
+            button_x_o_2_1.Enabled = f;
+            button_x_o_2_1.ForeColor = Color.Fuchsia;
+            button_x_o_2_1.Tag = 21;
+
+            button_x_o_2_2.BackColor =       c;
+            button_x_o_2_2.FlatStyle =  fs;
+            button_x_o_2_2.Text =       s;
+            button_x_o_2_2.Enabled = f;
+            button_x_o_2_2.ForeColor = Color.Fuchsia;
+            button_x_o_2_2.Tag = 22;
+
+            button_x_o_2_3.BackColor =      c;
+            button_x_o_2_3.FlatStyle =  fs;
+            button_x_o_2_3.Text =       s;
+            button_x_o_2_3.Enabled = f;
+            button_x_o_2_3.ForeColor = Color.Fuchsia;
+            button_x_o_2_3.Tag = 23;
+
+            button_x_o_3_1.BackColor =      c;
+            button_x_o_3_1.FlatStyle = fs;
+            button_x_o_3_1.Text =      s;
+            button_x_o_3_1.Enabled = f;
+            button_x_o_3_1.ForeColor = Color.Fuchsia;
+            button_x_o_3_1.Tag = 31;
+
+            button_x_o_3_2.BackColor =      c;
+            button_x_o_3_2.FlatStyle = fs;
+            button_x_o_3_2.Text =      s;
+            button_x_o_3_2.Enabled = f;
+            button_x_o_3_2.ForeColor = Color.Fuchsia;
+            button_x_o_3_2.Tag = 32;
+
+            button_x_o_3_3.BackColor =      c;
+            button_x_o_3_3.FlatStyle = fs;
+            button_x_o_3_3.Text =      s;
+            button_x_o_3_3.Enabled = f;
+            button_x_o_3_3.ForeColor = Color.Fuchsia;
+            button_x_o_3_3.Tag = 33;
+        }
+
+        private void checking()
+        {
+           
+            if (
+                (Convert.ToByte (button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_1_2.Tag) +  Convert.ToByte( button_x_o_1_3.Tag) == 3  || Convert.ToByte(button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_1_2.Tag) + Convert.ToByte(button_x_o_1_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_2_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_2_3.Tag) == 3 || Convert.ToByte(button_x_o_2_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_2_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_3_1.Tag) + Convert.ToByte(button_x_o_3_2.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 3 || Convert.ToByte(button_x_o_3_1.Tag) + Convert.ToByte(button_x_o_3_2.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 3 || Convert.ToByte(button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_3_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_1_3.Tag) == 3 || Convert.ToByte(button_x_o_3_1.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_1_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_2_1.Tag) + Convert.ToByte(button_x_o_3_1.Tag) == 3 || Convert.ToByte(button_x_o_1_1.Tag) + Convert.ToByte(button_x_o_2_1.Tag) + Convert.ToByte(button_x_o_3_1.Tag) == 6)
+                || (Convert.ToByte(button_x_o_1_3.Tag) + Convert.ToByte(button_x_o_2_3.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 3 || Convert.ToByte(button_x_o_1_3.Tag) + Convert.ToByte(button_x_o_2_3.Tag) + Convert.ToByte(button_x_o_3_3.Tag) == 6)
+                || (Convert.ToByte(button_x_o_1_2.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_3_2.Tag) == 3 || Convert.ToByte(button_x_o_1_2.Tag) + Convert.ToByte(button_x_o_2_2.Tag) + Convert.ToByte(button_x_o_3_2.Tag) == 6)
+               )
+            {
+                if (label_player.Text == "player1")
+                {
+                    label_winer_name.Text = "player2";
+                    MessageBox.Show("player2", "the winner");
+                }
+                    
+                else
+                {
+                    label_winer_name.Text = "player1";
+                    MessageBox.Show("player1", "the winner");
+                }
+
+                buttons(Color.HotPink, FlatStyle.Flat, "E", false);
+            }
+
+            if (end_game == 9)
+            {
+                buttons(Color.HotPink, FlatStyle.Flat, "E", false);
+                label_winer_name.Text = "NO WINNER";
+                MessageBox.Show("NO WINNER", "the winner");
+            }
+        }
+
+        private void chingbutton(Button b)
+        {
+            if (b.Text == "?")
+            {
+                 end_game++;
+                if (label_player.Text == "player1")
+                {
+                  b.Text = "X";
+                  b.ForeColor = System.Drawing.Color.Gold;
+                  label_player.Text = "player2";
+                  b.Tag = 1;
+                }
+
+                else
+                {
+                  b.Text = "O";
+                  b.ForeColor = System.Drawing.Color.Gold;
+                  label_player.Text = "player1";
+                  b.Tag = 2;
+                }
+                checking();
+            }
+        }
+
+       private void button_reset_gaime_Click(object sender, EventArgs e)
+        {
+            end_game = 0;
+            label_player.Text = "player1";
+            label_winer_name.Text = "prossing";
+            buttons(Color.Black, FlatStyle.Popup, "?", true);
+        }
+
+        private void button_x_o_Click(object sender, EventArgs e)
+        {
+            chingbutton((Button)sender);
+        }
+    }
+}
